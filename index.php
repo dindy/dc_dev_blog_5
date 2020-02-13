bc3816160543b1c061b004590148e66e7cb7e119
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-13 14:31:39 +0100

    Implémente les classes Response et Request

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
 
 // load and initialize any global libraries
 require_once 'model.php';
 require_once 'controllers.php';
 
 // route the request internally
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-echo $uri;
+$uri = $request->getPathInfo();
 
-if ('/dc_dev_blog_5/index.php' === $uri OR '/dc_dev_blog_5/' === $uri ) {
-    list_action();
-} elseif ('/dc_dev_blog_5/index.php/show' === $uri && isset($_GET['id'])) {
-    show_action($_GET['id']);
+// Routeur
+if ('/' === $uri ) {
+    $response = list_action();
+} elseif ('/show' === $uri && $request->query->has('id')) {
+    $response = show_action($request->query->get('id'));
 } else {
-    header('HTTP/1.1 404 Not Found');
-    echo '<html><body><h1>Page Not Found</h1></body></html>';
+    $html = '<html><body><h1>Page Not Found</h1></body></html>';
+    $response = new Response($html, 404);
 }
+
+$response->send();
 
 ?>
 
