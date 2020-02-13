49a94398d8f938ac57a7ade5b6744a590364e88e
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-13 10:35:58 +0100

    Isoler la présentation

---
+++ templates/list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>List of Posts</title>
+    </head>
+    <body>
+        <h1>List of Posts</h1>
+        <ul>
+            <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+            <li>
+                <a href="/show.php?id=<?php echo $row['id'] ?>">
+                    <?= $row['title'] ?>
+                </a>
+            </li>
+            <?php endwhile ?>
+        </ul>
+    </body>
+</html>
