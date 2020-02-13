07c4559df49082092b3d128887e47110e5dda2ce
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-13 11:51:28 +0100

    Page détails de post

--- model.php
+++ model.php
@@ -26,3 +26,19 @@ function get_all_posts()
 
     return $posts;
 }
+
+function get_post_by_id($id)
+{
+    $connection = open_database_connection();
+
+    $query = 'SELECT created, title, content FROM posts WHERE id=:id';
+    $statement = $connection->prepare($query);
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    close_database_connection($connection);
+
+    return $row;
+}
