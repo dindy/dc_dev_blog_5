d01f887492d317ce170db5dbaa17439773ff3b93
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-02-13 12:09:13 +0100

    Front controller

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
-require 'model.php';
+// load and initialize any global libraries
+require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+echo $uri;
 
-// include the HTML presentation code
-require 'templates/list.php';
+if ('/dc_dev_blog_5/index.php' === $uri OR '/dc_dev_blog_5/' === $uri ) {
+    list_action();
+} elseif ('/dc_dev_blog_5/index.php/show' === $uri && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
 
 ?>
 
